8f79a875719e74fd177b3b78a867c08959681208
coder1932010 <88028064+coder1932010@users.noreply.github.com>
2023-02-28 18:55:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump =loadSound("jump.wav");
+	mario_coin =loadSound("coin.wav");
+	mario_gameover =loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
+
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video =createCapture(VIDEO);
+	video.size(800 ,400);
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video , load);
+	poseNet.on('pose',gotposes);
+
+}
+
+function load(){
+	console.log('Model Loaded!');
+}
+
+function gotposes(result){
+	if(result.length > 0){
+		nosex=result[0].pose.nose.x;
+		nosey=result[0].pose.nose.y;
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
+
+
